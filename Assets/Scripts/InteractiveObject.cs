25fa6f8aed8ae0b44d5d78ed2e1beb8c63df31ab
behdad <behdadchalaki@gmail.com>
2021-04-24 14:48:18 -0400

    Text popup

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -1,22 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class InteractiveObject : MonoBehaviour
 {
+    public string[] msg; 
+    public Text popUpObj; 
+    bool popUp = false;
+    Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
         print("the interactive script is running!!");
+        cam = Camera.main;
     }
 
     // Update is called once per frame
+
     void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Player")
         {
-            print("Some thing entered");
-        }
-        
+            popUp = true;
+        }  
     }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            popUp = false; 
+
+        }
+    }
+    void Update()
+    {
+        if (popUp)
+        {
+            Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
+            popUpObj.text = msg[0]; //for now 
+            popUpObj.transform.position = screenPos;
+        }
+        else
+        {
+            popUpObj.text = "";
+        }
+            
+    }
+
 }
