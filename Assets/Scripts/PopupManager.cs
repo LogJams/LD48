94d48d67d895d71135d24c1ca498f11283f0e719
behdad <behdadchalaki@gmail.com>
2021-04-25 13:11:36 -0400

    Adding the Dialogue options

--- Assets/Scripts/PopupManager.cs
+++ Assets/Scripts/PopupManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
+//using System.Numerics;
 using UnityEngine;
 
 public class PopupManager : MonoBehaviour {
 
     public GameObject popupPanel;
+    public GameObject popUpButtons;
 
 
     public GameObject GetPopup() {
         return Instantiate(popupPanel, this.transform);
+    }
+    public GameObject[] GetPopupButtons(int number)
+    {
+        GameObject[] popUpButtonsArray = new GameObject[number];
+        for(int i=0; i<number; i++)
+        {
+            popUpButtonsArray[i] = Instantiate(popUpButtons, this.transform.position + Vector3.down*30*i, new Quaternion(), this.transform);
+        }
+        
+        return popUpButtonsArray;
     }
 
 }
