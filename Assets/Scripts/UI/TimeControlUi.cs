520e8e592195c6e689c8772afa425889f960e910
Logan <Logiant@users.noreply.github.com>
2021-04-26 16:17:12 -0400

    Minor fixes to Scenes 1 and 2

--- Assets/Scripts/UI/TimeControlUi.cs
+++ Assets/Scripts/UI/TimeControlUi.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System;
+
 using UnityEngine.SceneManagement;
 public class TimeControlUi : MonoBehaviour {
 
     public Image backIcon;
+    public GameObject backwardText;
+
     public Image forwardIcon;
+    public GameObject forwardText;
 
 
     //called only when the first level is loaded for the first time
@@ -18,8 +23,27 @@ public class TimeControlUi : MonoBehaviour {
         //configure scene 1
         backIcon.color = new Color(1, 1, 1, 0);
 
+        GameManager.instance.onUnlockScene += OnCrystalPickup;
+
     }
 
+
+    void OnCrystalPickup(System.Object sender, EventArgs args) {
+        if (GameManager.instance.CurrentScene == 1) {
+            //hide the back icon
+            backIcon.color = new Color(1, 1, 1, 0);
+            backwardText.SetActive(false);
+
+            //show the front icon if it's unlocked
+            if (GameManager.instance.UnlockedScene >= 2) {
+                forwardIcon.color = new Color(1, 1, 1, 1);
+                forwardText.SetActive(true);
+            } else {
+                forwardIcon.color = new Color(1, 1, 1, 0);
+                forwardText.SetActive(false);
+            }
+        }
+    }
 
     //our callback for updating the ui
     private void OnLevelLoad(Scene scene, LoadSceneMode sceneMode) {
@@ -27,28 +51,36 @@ public class TimeControlUi : MonoBehaviour {
         if (GameManager.instance.CurrentScene == 1) {
             //hide the back icon
             backIcon.color = new Color(1, 1, 1, 0);
+            backwardText.SetActive(false);
 
             //show the front icon if it's unlocked
             if (GameManager.instance.UnlockedScene >= 2) {
                 forwardIcon.color = new Color(1, 1, 1, 1);
+                forwardText.SetActive(true);
             } else {
                 forwardIcon.color = new Color(1, 1, 1, 0);
+                forwardText.SetActive(false);
             }
         }
 
         if (GameManager.instance.CurrentScene == 2) {
             backIcon.color = new Color(1, 1, 1, 1);
+            backwardText.SetActive(true);
             //show the front icon if it's unlocked
             if (GameManager.instance.UnlockedScene >= 3) {
                 forwardIcon.color = new Color(1, 1, 1, 1);
+                forwardText.SetActive(true);
             } else {
                 forwardIcon.color = new Color(1, 1, 1, 0);
+                forwardText.SetActive(false);
             }
         }
 
         if (GameManager.instance.CurrentScene == 3) {
             backIcon.color = new Color(1, 1, 1, 1);
+            backwardText.SetActive(true);
             forwardIcon.color = new Color(1, 1, 1, 0);
+            forwardText.SetActive(false);
         }
 
     }
