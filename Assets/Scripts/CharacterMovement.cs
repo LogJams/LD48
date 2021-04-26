0006c5c13f22e1399ce04975730c42b821554f05
Logan <Logiant@users.noreply.github.com>
2021-04-26 19:27:29 -0400

    Animated the player prefab

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,6 +8,7 @@ public class CharacterMovement : MonoBehaviour {
 
     public float speed = 5.0f;
 
+    public Animator anim;
 
     public float idleTime = 30f; // we are idle if no input for 30s;
 
@@ -36,11 +37,31 @@ public class CharacterMovement : MonoBehaviour {
 
         timer -= Time.deltaTime;
 
+
         Vector3 movement = new Vector3( Input.GetAxisRaw("Horizontal") / 2 + Input.GetAxisRaw("Vertical") / 2, 0, Input.GetAxisRaw("Vertical") / 2 - Input.GetAxisRaw("Horizontal") / 2);
 
         if (movement.sqrMagnitude > 0) {
+
+            //look towards mouse when moving
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo)) {
+                Vector3 lookPos = hitInfo.point;
+                lookPos.y = transform.position.y;
+                transform.LookAt(lookPos);
+            }
+
+
             timer = idleTime;
             movement.Normalize();
+            anim.SetFloat("X Speed", Input.GetAxis("Vertical")); // x speed
+            anim.SetFloat("Y Speed", Input.GetAxis("Horizontal")); // y speed
+            anim.SetFloat("Total Speed", 1.0f); //  total speed
+        }
+
+        else {
+            anim.SetFloat("X Speed", 0); // x speed
+            anim.SetFloat("Y Speed", 0); // y speed
+            anim.SetFloat("Total Speed", 0); //  total speed
         }
 
         cc.SimpleMove(movement * speed);
