9d556c280ed97d34c1586136a9fbb95ffaf7947b
Logan <Logiant@users.noreply.github.com>
2021-04-26 20:29:34 -0400

    fixed camera panning issue

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -13,6 +13,7 @@ public class MainCamera : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = player.position + boom;
     }
 
     // Update is called once per frame
